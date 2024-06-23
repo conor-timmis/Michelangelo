c7d55563d8e1d42fec0d942aa6403accc13c983e
Conor Timmis <mctto@protonmail.com>
2024-06-23 16:01:37 +0000

    Add Extends to Templates and Change Base

--- booking/settings.py
+++ booking/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'booking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
