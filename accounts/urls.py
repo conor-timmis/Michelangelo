121c2befc28432c546ffcdb50a685de1104f0500
Conor Timmis <mctto@protonmail.com>
2024-06-27 14:35:58 +0000

    Add registration and handling messages

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_login, name='login'),
+    path('user_login/', views.user_login, name="login"),
+    path('user_logout/', views.user_login, name = 'logout'),
+    path('registration', views.registration, name = 'registration'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages 
+from django.contrib.auth.forms import UserCreationForm
 
 def user_login(request):
     if request.method == "POST":
@@ -9,10 +10,34 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, ("you logged in!"))
             return redirect('index')
         else:
             messages.success(request, ("There was an error logging in"))
-            return redirect('menu')
+            return redirect('index')
 
     else:
         return render(request, 'authentication/login.html', {})
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, ("You're logged out!"))
+    print("Logged out")
+    return redirect('index')
+
+def registration(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You just registered!"))
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
+
+    return render(request, 'authentication/registration.html', {'form': form,})
