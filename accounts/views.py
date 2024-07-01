3b576090179e817a4fb832bdde4b7941a48a93ba
Conor Timmis <mctto@protonmail.com>
2024-07-01 16:34:21 +0000

    Delete Accounts App

--- accounts/views.py
+++
@@ -1,7 +0,0 @@
-from allauth.account.views import LoginView, SignupView
-
-class LoginView(LoginView):
-    template_name = 'my_login.html'
-
-class SignupView(SignupView):
-    template_name = 'my_signup.html'
