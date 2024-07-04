4a69aa250172e603cbe4d757a868c77e8b549187
Conor Timmis <mctto@protonmail.com>
2024-07-04 13:06:17 +0000

    Update .gitignore to exclude staticfiles directory

--- booking/settings.py
+++ booking/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = ('static',)
 
 # Default primary key field type
