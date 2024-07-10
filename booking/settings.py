77069e6581060027f3373113f089de0e2c31fcab
Conor Timmis <mctto@protonmail.com>
2024-07-10 11:39:45 +0000

    Revert Login for functionality

--- booking/settings.py
+++ booking/settings.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'booking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
