29f9b1262502523e1cc34c19e652db8ca26964bb
Conor Timmis <mctto@protonmail.com>
2024-07-17 17:20:02 +0000

    Add confirmation to Delete Booking page

---
+++ static/js/confirm.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var deleteButtons = document.querySelectorAll('input[type=submit]');
+    deleteButtons.forEach(function(button) {
+        button.onclick = confirmDelete;
+    });
+}
+
+function confirmDelete() {
+    return confirm("Are you sure you want to delete this booking?");
+}
