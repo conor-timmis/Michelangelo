dbaac1aea2a792cfd7966aefcb4a91e67a22ce14
Conor Timmis <mctto@protonmail.com>
2024-07-18 18:58:17 +0000

    Refactor Signup page and add Info Script

---
+++ static/js/signuplist.js
@@ -0,0 +1,8 @@
+window.toggleInfoList = function() {
+    var infoList = document.getElementById("infoList");
+    if (infoList.style.display === "none") {
+        infoList.style.display = "block";
+    } else {
+        infoList.style.display = "none";
+    }
+}
