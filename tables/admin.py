5e5108a177c23ea8c2ffcc32b49bf7681bbad2b3
Conor Timmis <mctto@protonmail.com>
2024-07-18 16:58:30 +0000

    Customise Django admin panel for Bookings

--- tables/admin.py
+++ tables/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Booking
+from .forms import BookingForm
 
 # Register your models here.
+
+class BookingAdmin(admin.ModelAdmin):
+    form = BookingForm
+    list_display = ('customer_name', 'meal_day', 'meal_time', 'number_of_guests', 'special_occasion')
+    search_fields = ('customer_name',)
+    list_filter = ('meal_day',)
 
 admin.site.register(Booking)
